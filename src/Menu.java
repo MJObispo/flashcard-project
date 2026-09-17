d28e2099c617bbf26e77a93120e3425fe2af8e20
MJ <mjobispo30@gmail.com>
2026-09-17 14:01:02 +1000

    Project init

---
+++ src/Menu.java
@@ -0,0 +1,9 @@
+public class Menu {
+     public static void printMenu() {
+        System.out.println("\n=== Flashcard App ===");
+        System.out.println("1. Add new flashcard");
+        System.out.println("2. View all flashcards");
+        System.out.println("0. Exit");
+        System.out.print("Choose an option: ");
+    }
+}
